c6d31c5c0f36f91d60154711104cc9a994e621eb
Karolina_Pakai <karolina_pakai@epam.com>
2021-07-06 16:10:12 +0200

    method implementations

--- gameoflife/src/main/java/com/epam/exercises/gameoflife/DefaultBoard.java
+++ gameoflife/src/main/java/com/epam/exercises/gameoflife/DefaultBoard.java
@@ -2,4 +2,19 @@ package com.epam.exercises.gameoflife;
 
 public class DefaultBoard implements Board {
 
+
+    @Override
+    public Board getNextGenerationBoard() {
+        return Board.super.getNextGenerationBoard();
+    }
+
+    @Override
+    public void insertCell(Coordinate coordinate) {
+        Board.super.insertCell(coordinate);
+    }
+
+    @Override
+    public boolean isAlive(Coordinate coordinate) {
+        return Board.super.isAlive(coordinate);
+    }
 }
