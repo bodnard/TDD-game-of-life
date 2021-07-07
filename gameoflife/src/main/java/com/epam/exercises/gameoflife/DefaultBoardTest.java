3e98add88d70c92ff38f07b24713b4328f8ae6d1
Daniel Bodnar <daniel_bodnar@epam.com>
2021-07-07 14:41:53 +0200

    test whenTwoNeighboursAndAliveThenSurvives

--- gameoflife/src/main/java/com/epam/exercises/gameoflife/DefaultBoardTest.java
+++ gameoflife/src/main/java/com/epam/exercises/gameoflife/DefaultBoardTest.java
@@ -85,4 +85,29 @@ public class DefaultBoardTest {
         assertFalse(result);
     }
 
+    @Test
+    public void whenTwoNeighboursAndAliveThenSurvives() {
+        //GIVEN
+        Coordinate coordinateCentral = new Coordinate(10, 10);
+        Coordinate coordinateNeighbour1 = new Coordinate(11, 10);
+        Coordinate coordinateNeighbour2 = new Coordinate(9, 10);
+        //WHEN
+        subject.insertCell(coordinateCentral);
+        subject.insertCell(coordinateNeighbour1);
+        subject.insertCell(coordinateNeighbour2);
+        boolean condition = subject.isAlive(coordinateCentral) &&
+            !subject.isAlive(new Coordinate(9, 9)) &&
+            subject.isAlive(coordinateNeighbour2) &&
+            !subject.isAlive(new Coordinate(9, 11)) &&
+            !subject.isAlive(new Coordinate(10, 9)) &&
+            !subject.isAlive(new Coordinate(10, 11)) &&
+            !subject.isAlive(new Coordinate(11, 9)) &&
+            subject.isAlive(coordinateNeighbour1) &&
+            !subject.isAlive(new Coordinate(11, 11));
+        assertTrue(condition);
+        boolean result = subject.getNextGenerationBoard().isAlive(coordinateCentral);
+        //THEN
+        assertTrue(result);
+    }
+
 }
